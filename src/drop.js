4b49041cbff08bbe0a194646a5f8970bcc81cd4b
Dante le Poole <dantelepoole@gmail.com>
2022-06-21 22:43:50 -0400

    Bugfix

--- src/drop.js
+++ src/drop.js
@@ -4,11 +4,38 @@
 
 'use strict';
 
-const negate = require('../lib/negate');
-const predicate = require('./predicate');
+const TRANSFORM_REJECT = require('../lib/transformrejectsymbol');
+
+const ERR_BAD_FILTER_TRANSFORMER = `The filter transformer has type %s. Expected a function.`;
+
+const fail = require('../lib/fail');
+const notfunction = require('../lib/notfunction');
+const type = require('../lib/type');
+
+const alwaysfalse = ()=>false;
 
 module.exports = function drop(...filters) {
 
-    const negatedfilters = filters.map(negate);
-    return predicate(...negatedfilters);
+    validatefilters(filters);
+
+    const filter = compose(filters);
+
+    return function filtertransformer(value) {
+        return filter(value) ? TRANSFORM_REJECT : value;
+    }
+}
+
+function compose(filters) {
+
+    return (filters.length === 0) ? alwaysfalse
+         : (filters.length === 1) ? filters[0]
+         : value => filters.every( filter => filter(value) );
+}
+
+function validatefilters(filters) {
+    filters.forEach(validate);
+}
+
+function validate(filter) {
+    if( notfunction(filter) ) fail(ERR_BAD_FILTER_TRANSFORMER, type(filter));
 }
